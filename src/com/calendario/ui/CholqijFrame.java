4aac8aec916dae8e3f259649f9a11e892890982c
JesfrinGnZ <jess2298gnz@gmail.com>
2019-08-22 00:08:49 -0600

    Se agrego informacion a las fechas

--- src/com/calendario/ui/CholqijFrame.java
+++ src/com/calendario/ui/CholqijFrame.java
@@ -14,7 +14,10 @@ public class CholqijFrame extends javax.swing.JFrame {
     public CholqijFrame() {
         initComponents();
         this.manejador = new ManejadorDeCalendarioCholqij();
+        this.infoTextArea.setText("Imox significa la esencia de nuestra conciencia y de nuestra mente. "+"\n"
+                + "Imox es el lado izquierdo, la parte sutil del ser humano. Imox es la sensibilidad, lo inusual y lo excéntrico.");
         this.setLocationRelativeTo(null);
+        
 
     }
 
@@ -29,6 +32,8 @@ public class CholqijFrame extends javax.swing.JFrame {
         nahualLabel = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        infoTextArea = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -68,57 +73,67 @@ public class CholqijFrame extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Liberation Mono", 2, 18)); // NOI18N
         jLabel2.setText("Nahual");
 
+        infoTextArea.setEditable(false);
+        infoTextArea.setBackground(new java.awt.Color(102, 102, 102));
+        infoTextArea.setColumns(20);
+        infoTextArea.setForeground(new java.awt.Color(0, 0, 0));
+        infoTextArea.setRows(5);
+        jScrollPane1.setViewportView(infoTextArea);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(130, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(anteriorButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(siguienteButton))
+                        .addGap(273, 273, 273)
+                        .addComponent(jLabel2)
+                        .addGap(27, 27, 27)
+                        .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(254, 254, 254)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(nahualLabel)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(20, 20, 20)))
+                            .addComponent(nahualLabel)
+                            .addComponent(anteriorButton))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
+                            .addComponent(siguienteButton)
                             .addComponent(energiaLabel))))
-                .addGap(119, 119, 119))
+                .addGap(0, 257, Short.MAX_VALUE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(42, Short.MAX_VALUE)
+                .addContainerGap(22, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(nahualLabel)
                     .addComponent(energiaLabel))
-                .addGap(40, 40, 40)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(anteriorButton)
                     .addComponent(siguienteButton))
-                .addGap(87, 87, 87))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -138,9 +153,11 @@ public class CholqijFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton anteriorButton;
     private javax.swing.JLabel energiaLabel;
+    private javax.swing.JTextArea infoTextArea;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel nahualLabel;
     private javax.swing.JButton siguienteButton;
     // End of variables declaration//GEN-END:variables
@@ -148,6 +165,7 @@ public class CholqijFrame extends javax.swing.JFrame {
     private void cambioDeDia(KinCholqij dia) {
         energiaLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource(dia.getEnergia().getDireccionDeImagen()))); // NOI18N
         nahualLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource(dia.getNahual().getDireccionDeImagen()))); // NOI18N
+        this.infoTextArea.setText(dia.getNahual().getDescripcion());
     }
 
 }
