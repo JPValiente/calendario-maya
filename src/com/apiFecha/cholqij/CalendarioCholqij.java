6f211aa8e65b66efa5e53de2193ad39c8ef71d65
Ricardo mendez <rikardomendez77@gmail.com>
2019-08-28 22:00:26 -0600

    Matriz Cholquij

--- src/com/apiFecha/cholqij/CalendarioCholqij.java
+++ src/com/apiFecha/cholqij/CalendarioCholqij.java
@@ -11,14 +11,37 @@ import com.apiFecha.elementoCalendarioCholqij.KinCholqij;
 public class CalendarioCholqij {
 
     //COnstantes de url
-    private final String INCIO_URL_DE_NUMERO = "/com/imagenesNumerosMayas/numero";
-    private final String INCIO_URL_DE_NAHUAL = "/com/imagenesNahualesMayas/Nahual";
+    private static final String INCIO_URL_DE_NUMERO = "/com/imagenesNumerosMayas/numero";
+    private static final String INCIO_URL_DE_NAHUAL = "/com/imagenesNahualesMayas/Nahual";
     //Elementos de calendario
     private final KinCholqij[][] diasDeCalendarioCholtij = new KinCholqij[20][13];
     private final EnergiaCholqij energias[];
     private final NahualCholqij nahuales[];
     private String informacionDeNahuales[];
-
+    
+    //informacion de nahuales
+    public static final String nahualImox = "Imox significa la esencia de nuestra conciencia y de nuestra mente. "+"\n"+"Imox es el lado izquierdo, la parte sutil del ser humano. Imox es la sensibilidad, lo inusual y lo excéntrico.";
+    public static final String nahualIq = "Iq simboliza el espíritu vital, el relámpago, la tempestad. Iq´ simboliza las corrientes del aire, la limpieza"+"\n"+" y la pureza del cristal. "+"\n"+"En este día nació el viento y así se origino la vida. Iq´ es el purificador del cuerpo, de la mente y del espíritu.";
+    public static final String nahualAqabal = "Aqabal significa el amanecer y el atardecer, la luz y la oscuridad. Aq´ab´al es el signo de la renovación,"+"\n"+" de las oportunidades y la dualidad."+"\n"+ " Este día es para pedir claridad, nuevas oportunidades, renovación de la vida y la estabilidad.";
+    public static final String nahualKat = "Kat significa la red para pescar y para guardar el maíz. K´at también significa los enredos y los problemas."+"\n"+"Kat es el día propicio para desenredar un problema y pedir por la abundancia.";
+    public static final String nahualKan = "Kan significa la serpiente emplumada. Kan es el movimiento, creador y formador del universo."+"\n"+ " Kan es la evolución humana, el desarrollo espiritual, la justicia, la verdad, la inteligencia y la paz.";
+    public static final String nahualKame ="Kame significa la muerte, que es lo único seguro, todos nacemos un día y luego volvemos al origen. Kame es la"+"\n"+" muerte para el mundo maya, es una energía positiva."+"\n"+ " Kame es un día especial para tener contacto con los ancestros.";
+    public static final String nahualKej = "Kej significa venado. Kej son los cuatro pilares que sostienen el cielo y la tierra. Kej son los cuatro puntos"+"\n"+" cardinales, los cuatro caminos. Kej es el conocimiento del poder a través de la existencia física. "+"\n"+ "La armonía es la mayor fuerza de este día.";
+    public static final String nahualQanil = "Q´anil significa germen, semilla, vida y creación. Q´anil es la creación del universo, en especial la vida de la madre"+"\n"+" naturaleza. "+"\n"+ "Q´anil es un día para pedir por la germinación y la vida.";
+    public static final String nahualToj = "Toj significa la divinidad del sol. Toj es el fuego del espíritu del Ajaw. "+"\n"+ "Toj es la nivelación con la justicia, la vida y la esperanza.";
+    public static final String nahualTzi = "Tz´i´ significa perro, secretario, escritura, justicia y autoridad. Tz´i´ es el guardián de la ley material y espiritual. "+"\n"+ "Tz´i´ representa la fidelidad, el orden y la exactitud. ";
+    public static final String nahualBatz = "B´atz´ simboliza el tiempo desarrollado, la evolución y la vida humana. B´atz´ representa el principio de la vida,"+"\n"+" el tiempo infinito de la inteligencia y la sabiduría. B´atz´ también simboliza los fenómenos cósmicos y "+"\n"+"la sabiduría original.";
+    public static final String nahualE = "E simboliza el camino del destino, el guía, el que nos lleva a un punto objetivo y preciso, la búsqueda de la "+"\n"+"realización en todas las situaciones, aspectos y manifestaciones de la vida.";
+    public static final String nahualAj ="Aj representa a las nuevas generaciones. Significa el cañaveral, la abundancia, la unidad, el poder divino, "+"\n"+"la clarividencia, las palabras sagradas. Aj es el amor a la humanidad, la telepatía, las señales del cuerpo "+"\n"+"y los sueños inesperados.";
+    public static final String nahualIx = "I´x significa el jaguar o el tigre, la energía felina. I´x representa a la mujer y el altar maya. I´x es la "+"\n"+"energía que protege a los cerros, planicies y montañas.\n" +"I’x es principio de la vitalidad, unión de los puntos vitales de todos los seres vivos";
+    public static final String nahualTzikin ="Tz´ikin significa pájaro guardián, la libertad, el mensajero, el tesoro, la suerte y el dinero. Tz´ikin es el mejor"+"\n"+" día para el amor, para pedir abundancia material y espiritual, tanto en lo particular como en lo comunitario.";
+    public static final String nahualAjmac ="Ajmaq significa el perdón y el pecado, representa lo más oscuro de la noche y el primer rayo de luz. Ajmaq es"+"\n"+" tierra, espíritu de los abuelos y sabiduría ancestral. Ajmaq es la conciencia planetaria y la energía exterior.";
+    public static final String nahualNoj ="No´j simboliza el movimiento del cielo y la tierra. No´j es la cueva o caverna, poder de la inteligencia o sabiduría. "+"\n"+"No´j también significa las ideas y pensamientos. No´j es un día propicio para pedir claridad en"+"\n"+" la toma de decisiones.";
+    public static final String nahualTijax ="Tijax simboliza el cuchillo de obsidiana, el cuchillo de doble filo. Tijax es la fuerza y el poder del guerrero. "+"\n"+"Tijax simboliza lo drástico, el sufrimiento, las revelaciones por medio de imágenes fugaces. Tijax es el"+"\n"+" poder del trueno y el rayo.";
+    public static final String nahualKawoq ="Kawoq significa la fuerza de la unión, la conciencia y el desarrollo del plan cósmico. Kawoq significa crecimiento,"+"\n"+" fertilidad, energía para la abundancia material y espiritual. Kawoq simboliza a la mujer, la esposa y a "+"\n"+"la Sagrada vara de la autoridad.";
+    public static final String nahualAjpu ="Ajpu es el día de la grandeza y de la fuerza de la vida. Ajpu son los cazadores seguros y certeros. "+"\n"+"Ajpu es apto para obtener la certeza, la seguridad, el valor y la energía para vencer los obstáculos.";
+    
+    
     public CalendarioCholqij(boolean esParaCuadricula) {
         energias = new EnergiaCholqij[13];
         nahuales = new NahualCholqij[20];
@@ -78,26 +101,26 @@ public class CalendarioCholqij {
 
     private String[] llenarInformacionDeNahuales() {
         String[] info = new String[20];
-        info[0] = "Imox significa la esencia de nuestra conciencia y de nuestra mente. "+"\n"+"Imox es el lado izquierdo, la parte sutil del ser humano. Imox es la sensibilidad, lo inusual y lo excéntrico.";
-        info[1] = "Iq simboliza el espíritu vital, el relámpago, la tempestad. Iq´ simboliza las corrientes del aire, la limpieza"+"\n"+" y la pureza del cristal. "+"\n"+"En este día nació el viento y así se origino la vida. Iq´ es el purificador del cuerpo, de la mente y del espíritu.";
-        info[2] = "Aqabal significa el amanecer y el atardecer, la luz y la oscuridad. Aq´ab´al es el signo de la renovación,"+"\n"+" de las oportunidades y la dualidad."+"\n"+ " Este día es para pedir claridad, nuevas oportunidades, renovación de la vida y la estabilidad.";
-        info[3] = "Kat significa la red para pescar y para guardar el maíz. K´at también significa los enredos y los problemas."+"\n"+"Kat es el día propicio para desenredar un problema y pedir por la abundancia.";
-        info[4] = "Kan significa la serpiente emplumada. Kan es el movimiento, creador y formador del universo."+"\n"+ " Kan es la evolución humana, el desarrollo espiritual, la justicia, la verdad, la inteligencia y la paz.";
-        info[5] = "Kame significa la muerte, que es lo único seguro, todos nacemos un día y luego volvemos al origen. Kame es la"+"\n"+" muerte para el mundo maya, es una energía positiva."+"\n"+ " Kame es un día especial para tener contacto con los ancestros.";
-        info[6] = "Kej significa venado. Kej son los cuatro pilares que sostienen el cielo y la tierra. Kej son los cuatro puntos"+"\n"+" cardinales, los cuatro caminos. Kej es el conocimiento del poder a través de la existencia física. "+"\n"+ "La armonía es la mayor fuerza de este día.";
-        info[7] = "Q´anil significa germen, semilla, vida y creación. Q´anil es la creación del universo, en especial la vida de la madre"+"\n"+" naturaleza. "+"\n"+ "Q´anil es un día para pedir por la germinación y la vida.";
-        info[8] = "Toj significa la divinidad del sol. Toj es el fuego del espíritu del Ajaw. "+"\n"+ "Toj es la nivelación con la justicia, la vida y la esperanza.";
-        info[9] = "Tz´i´ significa perro, secretario, escritura, justicia y autoridad. Tz´i´ es el guardián de la ley material y espiritual. "+"\n"+ "Tz´i´ representa la fidelidad, el orden y la exactitud. ";
-        info[10] = "B´atz´ simboliza el tiempo desarrollado, la evolución y la vida humana. B´atz´ representa el principio de la vida,"+"\n"+" el tiempo infinito de la inteligencia y la sabiduría. B´atz´ también simboliza los fenómenos cósmicos y "+"\n"+"la sabiduría original.";
-        info[11] = "E simboliza el camino del destino, el guía, el que nos lleva a un punto objetivo y preciso, la búsqueda de la "+"\n"+"realización en todas las situaciones, aspectos y manifestaciones de la vida.";
-        info[12] = "Aj representa a las nuevas generaciones. Significa el cañaveral, la abundancia, la unidad, el poder divino, "+"\n"+"la clarividencia, las palabras sagradas. Aj es el amor a la humanidad, la telepatía, las señales del cuerpo "+"\n"+"y los sueños inesperados.";
-        info[13] = "I´x significa el jaguar o el tigre, la energía felina. I´x representa a la mujer y el altar maya. I´x es la "+"\n"+"energía que protege a los cerros, planicies y montañas.\n" +"I’x es principio de la vitalidad, unión de los puntos vitales de todos los seres vivos";
-        info[14] = "Tz´ikin significa pájaro guardián, la libertad, el mensajero, el tesoro, la suerte y el dinero. Tz´ikin es el mejor"+"\n"+" día para el amor, para pedir abundancia material y espiritual, tanto en lo particular como en lo comunitario.";
-        info[15] = "Ajmaq significa el perdón y el pecado, representa lo más oscuro de la noche y el primer rayo de luz. Ajmaq es"+"\n"+" tierra, espíritu de los abuelos y sabiduría ancestral. Ajmaq es la conciencia planetaria y la energía exterior.";
-        info[16] = "No´j simboliza el movimiento del cielo y la tierra. No´j es la cueva o caverna, poder de la inteligencia o sabiduría. "+"\n"+"No´j también significa las ideas y pensamientos. No´j es un día propicio para pedir claridad en"+"\n"+" la toma de decisiones.";
-        info[17] = "Tijax simboliza el cuchillo de obsidiana, el cuchillo de doble filo. Tijax es la fuerza y el poder del guerrero. "+"\n"+"Tijax simboliza lo drástico, el sufrimiento, las revelaciones por medio de imágenes fugaces. Tijax es el"+"\n"+" poder del trueno y el rayo.";
-        info[18] = "Kawoq significa la fuerza de la unión, la conciencia y el desarrollo del plan cósmico. Kawoq significa crecimiento,"+"\n"+" fertilidad, energía para la abundancia material y espiritual. Kawoq simboliza a la mujer, la esposa y a "+"\n"+"la Sagrada vara de la autoridad.";
-        info[19] = "Ajpu es el día de la grandeza y de la fuerza de la vida. Ajpu son los cazadores seguros y certeros. "+"\n"+"Ajpu es apto para obtener la certeza, la seguridad, el valor y la energía para vencer los obstáculos.";
+        info[0] = nahualImox;
+        info[1] = nahualIq;
+        info[2] = nahualAqabal;
+        info[3] = nahualKat;
+        info[4] = nahualKan;
+        info[5] = nahualKame;
+        info[6] = nahualKej;
+        info[7] = nahualQanil;
+        info[8] = nahualToj;
+        info[9] = nahualTzi;
+        info[10] = nahualBatz;
+        info[11] = nahualE;
+        info[12] = nahualAj;
+        info[13] = nahualIx;
+        info[14] = nahualTzikin;
+        info[15] = nahualAjmac;
+        info[16] = nahualNoj;
+        info[17] = nahualTijax;
+        info[18] = nahualKawoq;
+        info[19] = nahualAjpu;
         return info;
     }
 
---
+++ src/com/apiFecha/cholqij/Nahuales.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apiFecha.cholqij;
+
+
+/**
+ *
+ * @author richard
+ */
+public enum Nahuales {
+    
+    
+    IMOX(CalendarioCholqij.nahualImox,1),
+    IQ(CalendarioCholqij.nahualIq,2),
+    AQABAL(CalendarioCholqij.nahualAqabal,3),
+    KAT(CalendarioCholqij.nahualKat,4),
+    KAN(CalendarioCholqij.nahualKan,5),
+    KAME(CalendarioCholqij.nahualKame,6),
+    KEJ(CalendarioCholqij.nahualKej,7),
+    QANIL(CalendarioCholqij.nahualQanil,8),
+    TOJ(CalendarioCholqij.nahualToj,9),
+    TZI(CalendarioCholqij.nahualTzi,10),
+    BATZ(CalendarioCholqij.nahualBatz,11),
+    E(CalendarioCholqij.nahualE,12),
+    AJ(CalendarioCholqij.nahualAj,13),
+    IX(CalendarioCholqij.nahualIx,14),
+    TZIKIN(CalendarioCholqij.nahualTzikin,15),
+    AJMAC(CalendarioCholqij.nahualAjmac,16),
+    NOJ(CalendarioCholqij.nahualNoj,17),
+    TIJAX(CalendarioCholqij.nahualTijax,18),
+    KAWOQ(CalendarioCholqij.nahualKawoq,19),
+    AJPU(CalendarioCholqij.nahualAjpu,20);
+    
+    private String info;
+    private int numeroNahual;
+    
+    private Nahuales(String info, int numeroNahual){
+        this.info = info;
+        this.numeroNahual = numeroNahual;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getNumeroNahual() {
+        return numeroNahual;
+    }
+
+    public void setNumeroNahual(int numeroNahual) {
+        this.numeroNahual = numeroNahual;
+    }
+    
+    
+}
